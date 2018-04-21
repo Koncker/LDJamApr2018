ed6b6b19eafeeb37a0a307e7c3c5390f44fc7ec4
Koncker <viki.rajani@gmail.com>
2018-04-22 00:43:31 +0100

    First Day Progress - Added main scene - Character - Dog - Text Scripts

---
+++ Assets/Scripts/FirstText.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FirstText : MonoBehaviour {
+
+    public float delay = 0.1f;
+    public string fullText;
+    private string currentText = "";
+    public GameObject Text;
+    public AudioSource gunshot;
+
+    private void Start()
+    {
+        StartCoroutine(ShowText());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            gunshot.Play();
+            /* Destroy(Text); */
+        }
+    }
+
+    IEnumerator ShowText()
+    {
+        for(int i = 0; i < fullText.Length; i++)
+        {
+            currentText = fullText.Substring(0, i);
+            this.GetComponent<Text>().text = currentText;
+            yield return new WaitForSeconds(delay);
+        }
+    }
+
+}
---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour {
+
+    public GameObject startScreen;
+    public GameObject game;
+    public GameObject player;
+    public GameObject story;
+    public GameObject dog;
+
+    private bool waitingToStartGame = true;
+    public bool AllowStart = true;
+
+
+	void Start ()
+    {
+		if (startScreen != null)
+        {
+            startScreen.SetActive(true);
+        }
+        else
+        {
+            waitingToStartGame = false;
+            Debug.LogError("startScreen was not set in the inspector.");
+        }
+
+        if (game != null)
+        {
+            game.SetActive(false);
+        }
+        else
+        {
+            Debug.LogError("game was not set in the inspector.");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (AllowStart && waitingToStartGame && (Input.anyKey))
+        {
+            waitingToStartGame = false;
+            if (startScreen != null)
+            {
+                startScreen.SetActive(false);
+            }
+            if (game != null)
+            {
+                game.SetActive(true);
+                player.SetActive(true);
+                story.SetActive(true);
+                dog.SetActive(true);
+                Time.timeScale = 1;
+            }
+        }
+	}
+}
